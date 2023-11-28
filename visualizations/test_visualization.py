7dfe8c2bc68ad363f41b4fd65f9add25527ca206
Sarthak Mohanty <smohanty61@gatech.edu>
2023-11-28 18:03:31 -0500

    added visualizations

--- visualizations/test_visualization.py
+++ visualizations/test_visualization.py
@@ -1,14 +1,28 @@
-from dgl.data import CoraGraphDataset, CiteseerGraphDataset
+from torch_geometric.datasets import FacebookPagePage, Twitch
+import torch_geometric.transforms as T
+from torch_geometric.utils import to_dgl
 from gnnlens import Writer
+import os
 
-cora_dataset = CoraGraphDataset()
-cora_graph = cora_dataset[0]
-citeseer_dataset = CiteseerGraphDataset()
-citeseer_graph = citeseer_dataset[0]
+data_dir = './data'
+facebook_dataset = FacebookPagePage(os.path.join(data_dir, 'FacebookPagePage'), transform=T.NormalizeFeatures())
+facebook_graph = facebook_dataset[0]
+facebook_labels = facebook_graph.y
+facebook_num_classes = facebook_dataset.num_classes
+facebook_dgl_graph = to_dgl(facebook_graph)
 
-# Specify the path to create a new directory for dumping data files.
-writer = Writer('graphs/test_graph')
-writer.add_graph(name='Cora', graph=cora_graph)
-writer.add_graph(name='Citeseer', graph=citeseer_graph)
-# Finish dumping
+twitch_dataset = Twitch(os.path.join(data_dir, 'Twitch'), transform=T.NormalizeFeatures(), name='DE')
+twitch_graph = twitch_dataset[0]
+twitch_labels = twitch_graph.y
+twitch_num_classes = twitch_dataset.num_classes
+twitch_dgl_graph = to_dgl(twitch_graph)
+
+print(twitch_graph)
+print(facebook_graph)
+
+writer = Writer('graphs/unlearning_graphs')
+writer.add_graph(name='Facebook', graph=facebook_dgl_graph,
+                 nlabels=facebook_labels, num_nlabel_types=facebook_num_classes)
+writer.add_graph(name='Twitch', graph=twitch_dgl_graph,
+                    nlabels=twitch_labels, num_nlabel_types=twitch_num_classes)
 writer.close()
